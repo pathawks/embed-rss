b67b79ee32679633afa49d826d0f1bc1482961cb
Pat Hawks <pat@pathawks.com>
2011-03-03 17:24:54 +0000

    Adding first version
    git-svn-id: http://plugins.svn.wordpress.org/dirtysuds-embed-rss/trunk@354801 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ embed.php
@@ -0,0 +1,66 @@
+<?php
+/*
+Plugin Name: DirtySuds - Embed RSS
+Plugin URI: http://dirtysuds.com
+Description: Embed a RSS feed
+Author: Pat Hawks
+Version: 1.00
+Author URI: http://www.pathawks.com
+
+Updates:
+1.00 20110224 - First Version
+
+  Copyright 2011 Pat Hawks  (email : pat@pathawks.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+wp_embed_register_handler( 'rss', '#(^http.+((\.|=)(rss|atom|xml)|\/feed\/)$)#i', 'wp_embed_handler_rss', 20 );
+function wp_embed_handler_rss( $matches, $attr, $url, $rawattr ) {
+	include_once(ABSPATH . WPINC . '/feed.php');
+	// Get a SimplePie feed object from the specified feed source.
+	$rss = fetch_feed($matches[0]);
+	if (!is_wp_error( $rss ) ) : // Checks that the object is created correctly 
+	    // Figure out how many total items there are, but limit it to 4. 
+	    $maxitems = $rss->get_item_quantity(4); 
+	
+	    // Build an array of all the items, starting with element 0 (first element).
+	    $rss_items = $rss->get_items(0, $maxitems);
+	else:
+		return "error";
+	endif;
+	
+	$embed = '<ul>';
+	if ($maxitems == 0)
+		$embed .= '<li>No items.</li>';
+	else
+	    // Loop through each feed item and display each item as a hyperlink.
+	    foreach ( $rss_items as $item ) :
+		    $embed .= '<li><a href="'.$item->get_permalink().'" title="Posted '.$item->get_date('j F Y | g:i a').'">';
+		    $embed .= $item->get_title().'</a></li>';
+		endforeach;
+	$embed .= '</ul>';
+	
+	return apply_filters( 'embed_rss', $embed, $matches, $attr, $url, $rawattr  );
+}
+
+
+add_filter('plugin_row_meta', 'dirtysuds_embed_rss_rate',10,2);
+function dirtysuds_embed_rss_rate($links,$file) {
+		if (plugin_basename(__FILE__) == $file) {
+			$links[] = '<a href="http://wordpress.org/extend/plugins/dirtysuds-embed-rss/">Rate this plugin</a>';
+		}
+	return $links;
+}
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== DirtySuds - Embed RSS ===
+Contributors: pathawks
+Donate link: http://www.pathawks.com/p/wordpress-plugins.html
+Tags: plugins, wordpress, embed, eombed, rss
+Requires at least: 2.7
+Tested up to: 3.1
+Stable tag: 0.98
+
+Adds pseudo oembed support for RSS feeds
+
+== Description ==
+
+Will add a list of links to items in a given feed.
+Simply include the URL for an RSS feed on it's own line, or wrapped in the embed tag like `[embed]http://example.com/feed.rss[/embed]` and the plugin will embed the feed the page with links to each item.
+The url must end with `.rss`
+
+== Installation ==
+
+1. Upload `dirtysuds-embed-rss` to the `/wp-content/plugins/` directory
+2. Activate **DirtySuds - Embed PDF** through the 'Plugins' menu in WordPress
+3. That's it. Now when you embed an RSS feed using the Wordpress `[embed]` shortcode, the plugin will embed the feed
+
+
+== Frequently Asked Questions ==
+
+= I have an idea for a great way to improve this plugin =
+
+Great! I'd love to hear from you.
+postlist@pathawks.com
+
+
+== Changelog ==
+
+= 0.98 =
+* First version
